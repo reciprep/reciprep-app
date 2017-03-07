7601df5ba6096e15b6757c00564e06737514a665
nickkoul <nickkoul13@gmail.com>
2017-03-07 16:40:18 -0500

    initial login design

--- app/routes/LogIn.js
+++ app/routes/LogIn.js
@@ -7,7 +7,7 @@ someFunction = () => {
 };
 
 loginFunction = ()=> {
-  Alert.alert('Button has been pressed!');
+  // Alert.alert('Button has been pressed!');
 }
 
 export class LogIn extends Component {
@@ -17,29 +17,45 @@ export class LogIn extends Component {
         <View style={styles.loginHeader}>
           <Text style={styles.headerText}>ReciPrep</Text>
         </View>
-        <View style={styles.inputGroup}>
-          <FormLabel labelStyle={styles.inputLabel}>UserName</FormLabel>
-          <FormInput inputStyle={styles.inputText} onChangeText={someFunction}/>
+        <View style={styles.loginView}>
+          <View style={styles.inputGroup}>
+            <FormLabel labelStyle={styles.inputLabel}>UserName</FormLabel>
+            <FormInput inputStyle={styles.inputText} onChangeText={someFunction}/>
+          </View>
+          <View style={styles.inputGroup}>
+            <FormLabel labelStyle={styles.inputLabel}>Password</FormLabel>
+            <FormInput inputStyle={styles.inputText} onChangeText={someFunction}/>
+          </View>
+          <Button
+            title='Login'
+            buttonStyle={styles.loginButton}
+            raised
+            underlayColor = 'blue'
+            onPress={loginFunction}/>
+          <Button
+            title='Signup'
+            buttonStyle={styles.signupButton}
+            raised
+            onPress={loginFunction}/>
         </View>
-        <View style={styles.inputGroup}>
-          <FormLabel labelStyle={styles.inputLabel}>Password</FormLabel>
-          <FormInput inputStyle={styles.inputText} onChangeText={someFunction}/>
-        </View>
-        <Button
-          title='Login'
-          raised
-          onPress={loginFunction}/>
       </View>
 
     );
   }
 }
 
-//BackgroundColor 30415D,015249, $ABDAC
+//BackgroundColor 30415D,015249, 4ABDAC
 var styles = StyleSheet.create({
   page:{
     backgroundColor: '#4ABDAC',
     flex: 1,
+  },
+  loginHeader:{
+    flex:1,
+    justifyContent: 'center'
+  },
+  loginView:{
+    flex:3,
     // justifyContent: 'center'
   },
   headerText:{
@@ -51,13 +67,27 @@ var styles = StyleSheet.create({
   inputGroup:{
     backgroundColor:'#DFDCE3',
     margin: 20,
-    height: 70
+    height: 70,
+    borderRadius: 7,
+    elevation: 2
   },
   inputLabel:{
     color: '#000000',
   },
   inputText:{
     color: '#000000'
+  },
+  loginButton:{
+    marginTop: 30,
+    width: 300,
+    borderRadius: 7,
+    alignSelf: 'center'
+  },
+  signupButton:{
+    marginTop: 20,
+    width: 300,
+    borderRadius: 7,
+    alignSelf: 'center'
   }
 });
 
