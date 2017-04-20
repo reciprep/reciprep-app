72550367be83c9d7c076e82d2f643bf271caf7c0
clopeckm <clopeckm@gmail.com>
2017-04-20 16:26:38 -0400

    add new ingredient to pantry

--- app/routes/pantry/pantryPage.js
+++ app/routes/pantry/pantryPage.js
@@ -116,11 +116,11 @@ export class PantryPage extends React.Component {
           .then( (response) => response.json())
           .then( (responseData) => {
             if(responseData['status'] == 'success'){
-              Alert.alert("Ingredient Quantity Chaged")
+              Alert.alert("Ingredient Quantity Changed")
               console.log('Create request succeeded', responseData);
             }
             else{
-              Alert.alert("Ingredient change faileD")
+              Alert.alert("Ingredient change failed")
               console.log('Request failed', responseData);
             }
           }).catch( (error) =>{
@@ -147,15 +147,43 @@ export class PantryPage extends React.Component {
 
 
   //the below modal will save the data
-  _closeModal3 = () =>{
+  _closeModal3 = async () =>{
     this.setState({modalVisible2: false});
     var count;
     var count2;
+    var jsonObject = {ingredient_name: this.state.ingredientType, 'value': parseFloat(this.state.quantity)}
+    var jsonObjectArr = [];
+
     for(count=0;count<8; count++){
       //below means we matched our category to an ingredient
       if(this.state.category2 == this.props.data[count].title){
         this.props.data[count].subitems.push({title:this.state.ingredientType ,icon:'Oil',value:this.state.quantity});
         this.setState({dataSource: this.state.dataSource.cloneWithRows(this.props.data)});
+        let auth_token = "Bearer " + await AsyncStorage.getItem('auth_token');
+        fetch('http://10.0.2.2:8000/api/user/pantry',{
+          method: 'PATCH',
+          headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json',
+            'Authorization': auth_token
+          },
+          body: JSON.stringify({
+            'ingredients': jsonObjectArr
+          })
+        })
+        .then( (response) => response.json())
+        .then( (responseData) => {
+          if(responseData['status'] == 'success'){
+            Alert.alert("Ingredient Quantity Changed")
+            console.log('Create request succeeded', responseData);
+          }
+          else{
+            Alert.alert("Ingredient change failed")
+            console.log('Request failed', responseData);
+          }
+        }).catch( (error) =>{
+          console.error(error);
+        });
       }
     }
   };
@@ -321,33 +349,81 @@ export class PantryPage extends React.Component {
             <View style={styles.innerContainer}>
               <Text style={{fontSize: 16, fontWeight: 'bold',}}>Add Ingredient to Pantry</Text>
               <View style={{flex: 1, flexDirection: 'row', marginBottom:35}}>
-                <Text style={{fontSize: 14, fontWeight: 'bold',}}>Category:</Text>
-                <Picker
-                  style={styles.pickerText}
-                  selectedValue={this.state.category2}
-                  mode="dropdown"
-                  onValueChange={(text3) => this.setState({category2: text3})}>
-                  <Item label="Meats" value="MEATS"/>
-                  <Item label="Grains" value="GRAINS"/>
-                  <Item label="Fruits" value="FRUITS"/>
-                  <Item label="Vegetables" value="VEGETABLES"/>
-                  <Item label="Wet" value="WET"/>
-                  <Item label="Dry" value="DRY"/>
-                  <Item label="Dairy" value="DAIRY"/>
-                  <Item label="Misc" value="MISC"/>
-                </Picker>
-              </View>
-              <View style={{flex: 1, flexDirection: 'row', marginBottom:35}}>
                 <Text style={{fontSize: 14, fontWeight: 'bold',}}>Ingredient:</Text>
                 <Picker
                   style={styles.pickerText}
                   selectedValue={this.state.ingredientType}
                   mode="dropdown"
                   onValueChange={(text2) => this.setState({ingredientType: text2})}>
-                  <Item label="Apples" value="Apples"/>
-                  <Item label="Bannanas" value="Bannanas"/>
-                  <Item label="Oranges" value="Oranges"/>
-                  <Item label="Grapes" value="Grapes"/>
+                  <Item label = "garbanzo beans" value="garbanzo beans"/>
+                  <Item label = "artichoke hearts" value="artichoke hearts"/>
+                  <Item label = "garlic" value="garlic"/>
+                  <Item label = "tahini" value="tahini"/>
+                  <Item label = "extra virgin olive oil" value="extra virgin olive oil"/>
+                  <Item label = "water" value="water"/>
+                  <Item label = "kosher salt" value="kosher salt"/>
+                  <Item label = "eggs" value="eggs"/>
+                  <Item label = "mayonnaise" value="mayonnaise"/>
+                  <Item label = "Dijon mustard" value="Dijon mustard"/>
+                  <Item label = "white wine vinegar" value="white wine vinegar"/>
+                  <Item label = "chipotle chile powder" value="chipotle chile powder"/>
+                  <Item label = "garlic powder" value="garlic powder"/>
+                  <Item label = "thick-cut bacon" value="thick-cut bacon"/>
+                  <Item label = "crean cheese" value="crean cheese"/>
+                  <Item label = "brown suger" value="brown suger"/>
+                  <Item label = "pure maple syrup" value="pure maple syrup"/>
+                  <Item label = "canned pumkin" value="canned pumkin"/>
+                  <Item label = "pumkin pie spice" value="pumkin pie spice"/>
+                  <Item label = "cinnamon" value="cinnamon"/>
+                  <Item label = "vanilla extract" value="vanilla extract"/>
+                  <Item label = "whole wheat flour" value="whole wheat flour"/>
+                  <Item label = "oat bran" value="oat bran"/>
+                  <Item label = "pecan meal" value="pecan meal"/>
+                  <Item label = "baking powder" value="baking powder"/>
+                  <Item label = "baking soda" value="baking soda"/>
+                  <Item label = "salt" value="salt"/>
+                  <Item label = "butter" value="butter"/>
+                  <Item label = "honey" value="honey"/>
+                  <Item label = "egg whites" value="egg whites"/>
+                  <Item label = "banana" value="banana"/>
+                  <Item label = "buttermilk" value="buttermilk"/>
+                  <Item label = "pomegranate juice" value="pomegranate juice"/>
+                  <Item label = "balsamic vinegar" value="balsamic vinegar"/>
+                  <Item label = "vegetable oil" value="vegetable oil"/>
+                  <Item label = "baby spinach leaves" value="baby spinach leaves"/>
+                  <Item label = "apple" value="apple"/>
+                  <Item label = "pomegranate seeds" value="pomegranate seeds"/>
+                  <Item label = "toasted walnuts" value="toasted walnuts"/>
+                  <Item label = "cooked crumbled bacon" value="cooked crumbled bacon"/>
+                  <Item label = "blue cheese" value="blue cheese"/>
+                  <Item label = "pomegranate vinaigrette" value="pomegranate vinaigrette"/>
+                  <Item label = "onion" value="onion"/>
+                  <Item label = "carrots" value="carrots"/>
+                  <Item label = "hot peppers" value="hot peppers"/>
+                  <Item label = "garlic cloves" value="garlic cloves"/>
+                  <Item label = "potatoes" value="potatoes"/>
+                  <Item label = "chicken stock" value="chicken stock"/>
+                  <Item label = "spinach" value="spinach"/>
+                  <Item label = "parsley" value="parsley"/>
+                  <Item label = "thyme" value="thyme"/>
+                  <Item label = "cream" value="cream"/>
+                  <Item label = "cream cheese" value="cream cheese"/>
+                  <Item label = "blue cheese" value="blue cheese"/>
+                  <Item label = "onion powder" value="onion powder"/>
+                  <Item label = "ground beef" value="ground beef"/>
+                  <Item label = "salmon fillet" value="salmon fillet"/>
+                  <Item label = "capers" value="capers"/>
+                  <Item label = "salt" value="salt"/>
+                  <Item label = "black pepper" value="black pepper"/>
+                  <Item label = "lemon" value="lemon"/>
+                  <Item label = "linguine pasta" value="linguine pasta"/>
+                  <Item label = "shallots" value="shallots"/>
+                  <Item label = "shrimp" value="shrimp"/>
+                  <Item label = "dry white wine" value="dry white wine"/>
+                  <Item label = "wheat germ" value="wheat germ"/>
+                  <Item label = "blueberries" value="blueberries"/>
+                  <Item label = "olive oil" value="olive oil"/>
+
                 </Picker>
               </View>
               <View style={{flex: 1, flexDirection: 'row', marginBottom:35}}>
